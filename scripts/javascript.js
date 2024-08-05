30c623df2180a3c5abd8296016b5ac54c490a665
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-05 14:40:16 +0100

    Make it so user input changes the displayed text
    The buttons now change what's displayed on the calculator, adding the
    value of the button that they pressed on, whether it's an operand or an
    operator.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,16 @@
+if (document.readyState === "loading" ) {
+    document.addEventListener("DOMContentLoaded", initialize);
+
+} else {
+    initialize();
+}
+
+function initialize() {
+    display = document.querySelector(".display")
+    buttonsContainer = document.querySelector(".calculator-buttons");
+    buttonsContainer.addEventListener("click", parseInput)
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -26,5 +39,20 @@ function operate(num1, operator, num2) {
         case '/':
             return divide(num1, num2);
         }
+}
+
+let displayValue = ""
+let display;
+let buttonsContainer;
+
+function updateDisplay(value){
+    displayValue += value;
+    display.textContent = displayValue;
+}
+
+function parseInput(event) {
+    const target = event.target;
+    const targetContent = target.textContent;
+    updateDisplay(targetContent)
 }
 
