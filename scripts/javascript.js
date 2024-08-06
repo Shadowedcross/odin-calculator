46abaeea2e2e0b9b42dbc975485349c9f9abe26e
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-06 12:45:53 +0100

    Add functions to allow user to perform operations
    Added all the functions and variables necessary to display the result of
     the user's input. Included functionality includes:
     1. Basic mathematical operations with addition, subtraction,
     multiplication, and division
     2. Making sure operations are only performed between a pair of values
     3. Rounding values to two decimal values if not an integer, otherwise
     removing decimals
     4. Allowing user to press '=' before completing an operation, without
     causing issues
     5. Clearing data with the 'clr' button
     6. Displaying a special message if the user tries to divide by zero

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,16 @@
-if (document.readyState === "loading" ) {
+const DIVIDE_BY_ZERO_MSG = "To infinity and beyond!"
+
+let dividedByZero = false;
+let pastFirstOperator = false
+let num1 = 0
+let operationIndex = 0;
+let operationsArray = [];
+let displayValue = ""
+
+let display;
+let buttonsContainer;
+
+if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", initialize);
 
 } else {
@@ -9,6 +21,115 @@ function initialize() {
     display = document.querySelector(".display")
     buttonsContainer = document.querySelector(".calculator-buttons");
     buttonsContainer.addEventListener("click", parseInput)
+}
+
+function parseInput(event) {
+    const target = event.target;
+    const value = target.textContent;
+    if (dividedByZero || value === "clr") {
+        clear()
+    } else if (value === "=") {
+        if (operationsArray.length !== 0) {
+            calculate()
+        }
+    } else {
+        const type = Number.isInteger(+value) ? "operand" : "operator";
+        if (type === "operator") {
+            if (pastFirstOperator) {
+                calculate()
+            } else {
+                pastFirstOperator = true
+            }
+        }
+        updateOperationsArray(value, type)
+        addToDisplay(type === "operand" ? `${value}` : ` ${value} `)
+    }
+}
+
+function isInteger(value) {
+    return Number.isInteger(+value)
+}
+
+function updateOperationsArray(value, type) {
+    let indexValue = operationsArray[operationIndex]
+    if (indexValue === undefined) {
+        operationsArray.push(0)
+        indexValue = operationsArray[operationIndex]
+    }
+    const valueToPush = (type === "operand") ? +(indexValue + value) : value;
+    if (type === "operand") {
+        addOrEditOperand(valueToPush)
+    } else {
+        addOperator(value)
+
+    }
+}
+
+function addOperator(value) {
+    operationIndex++
+    operationsArray.push(value)
+    operationIndex++
+}
+
+function addOrEditOperand(value) {
+    operationsArray[operationIndex] = value;
+}
+
+function addToDisplay(value) {
+    displayValue = displayValue === "0" ? value : displayValue += value
+    display.textContent = displayValue;
+}
+
+function setDisplay(value) {
+    displayValue = value;
+    display.textContent = displayValue
+}
+
+function calculate() {
+    let operator = ""
+    let num2 = null
+    if (operationsArray.length > 0) {
+        setDisplay(operationsArray.reduce((sum, value) => {
+            if (isInteger(+value)) {
+                num2 = value;
+            } else {
+                operator = value;
+            }
+            if (isValidOperation(sum, operator, num2)) {
+                let result = operate(sum, operator, num2);
+                if (result === DIVIDE_BY_ZERO_MSG) {
+                    dividedByZero = true;
+                    return result;
+                } else {
+                    if (isInteger(result)) {
+                        sum = Math.floor(result);
+                    } else {
+                        sum = result.toFixed(2)
+                    }
+                    num1 = sum;
+                    operator = "";
+                    num2 = null;
+                }
+            }
+            return sum;
+        }))
+    }
+}
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2).toFixed(2);
+        case '-':
+            return subtract(num1, num2).toFixed(2);
+        case 'x':
+            return multiply(num1, num2).toFixed(2);
+        case '÷':
+            if (num2 === 0) {
+                return "To infinity and beyond!"
+            }
+            return divide(num1, num2).toFixed(2);
+    }
 }
 
 function add(num1, num2) {
@@ -27,32 +148,30 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, operator, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case 'x':
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
-        }
+function isValidOperation(num1, operator, num2) {
+    return (num1 !== null && operator !== "" && num2 != null)
 }
 
-let displayValue = ""
-let display;
-let buttonsContainer;
-
-function updateDisplay(value){
-    displayValue += value;
-    display.textContent = displayValue;
+function clear() {
+    num1 = 0
+    operationsArray = [];
+    operationIndex = 0;
+    dividedByZero = false
+    setDisplay("0")
 }
 
-function parseInput(event) {
-    const target = event.target;
-    const targetContent = target.textContent;
-    updateDisplay(targetContent)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
