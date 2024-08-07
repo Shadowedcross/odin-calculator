b0733f865c3b256f79aa0d7139e12846e7219177
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-07 19:23:51 +0100

    Refactor code, style, add features
    I reworked the code to make it easier to work with. While I was doing
    so, I added the ability to backspace, use periods to designate decimals,
    as well as letting the user use the keyboard to control the calculator.
    I also stylized the display to make it look nice. Since the display can
    expand to show bigger numbers, I offset the calculator from the bottom,
    in such a way that the buttons don't change position as the display gets
    bigger.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,14 +1,19 @@
-const DIVIDE_BY_ZERO_MSG = "To infinity and beyond!"
-
-let dividedByZero = false;
-let pastFirstOperator = false
-let num1 = 0
-let operationIndex = 0;
-let operationsArray = [];
-let displayValue = ""
+const DIVIDE_BY_ZERO_MESSAGES =[
+    "I'm sorry User. I'm afraid I can't do that.",
+    "To Infinity and Beyond!",
+    "We don't do that around these parts.",
+    "Divide by zero? Only in a parallel universe.",
+]
+let dividedByZero = false
 
 let display;
 let buttonsContainer;
+let body
+
+let currentOp = [null, null, null]
+let opsArray = []
+let opsIndex = 0
+let lastType
 
 if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", initialize);
@@ -18,146 +23,325 @@ if (document.readyState === "loading") {
 }
 
 function initialize() {
-    display = document.querySelector(".display")
+    body = document.querySelector("body")
+    body.addEventListener("keydown", parseInput)
+    display = document.querySelector(".display-text")
     buttonsContainer = document.querySelector(".calculator-buttons");
     buttonsContainer.addEventListener("click", parseInput)
 }
 
 function parseInput(event) {
+    let type
+    let value;
     const target = event.target;
-    const value = target.textContent;
-    if (dividedByZero || value === "clr") {
-        clear()
-    } else if (value === "=") {
-        if (operationsArray.length !== 0) {
-            calculate()
-        }
-    } else {
-        const type = Number.isInteger(+value) ? "operand" : "operator";
-        if (type === "operator") {
-            if (pastFirstOperator) {
-                calculate()
-            } else {
-                pastFirstOperator = true
+    if (dividedByZero) {
+        display.style.textAlign = "end"
+        dividedByZero = false
+    }
+    if (event.type === "keydown") {
+        let key = event.key
+        if (isNumber(key)) {
+            type = "operand"
+            value = key;
+        } else {
+            switch(key) {
+                case "Enter":
+                case " ":
+                    type = "calculate";
+                    break;
+                case "Backspace":
+                    type = "delete";
+                    break;
+                case "Delete":
+                    type = "clear";
+                    break;
+                case ".":
+                    type = "decimal";
+                    break;
+                case "%":
+                    type = "percent";
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "x":
+                case "/":
+                case "÷":
+                    type = "operator";
+                    if (key === "*") {
+                        value = "x"
+                    } else if (key === "/") {
+                        value = "÷"
+                    } else {
+                        value = key;
+                    }
+                    break;
             }
         }
-        updateOperationsArray(value, type)
-        addToDisplay(type === "operand" ? `${value}` : ` ${value} `)
+    } else if (target.type === "submit") {
+        value = target.textContent;
+        if (isNumber(value)) {
+            type = "operand"
+        } else {
+            switch (value) {
+                case "=":
+                    type = "calculate";
+                    break;
+                case "DEL":
+                    type = "delete";
+                    break;
+                case "C":
+                    type = "clear";
+                    break;
+                case ".":
+                    type = "decimal";
+                    break;
+                case "%":
+                    type = "percent";
+                    break;
+                default:
+                    type = "operator";
+                    break;
+            }
+        }
     }
+    if (type !== undefined) {
+        switch (type) {
+            case "operand":
+                if (lastType === "calculate") {
+                    clear()
+                }
+                addOperand(value);
+                break;
+            case "operator":
+                if (currentOp[2] != null) {
+                    calculate()
+                }
+                addOperator(value)
+                break;
+            case "decimal":
+                if (opsArray[opsIndex] === undefined || (opsArray[opsIndex] !== undefined && !opsArray[opsIndex].includes(".")) || opsArray[opsIndex] !== undefined) {
+                    if (currentOp[0] !== null && currentOp[1] !== null) {
+                        addDecimal(2)
+                    } else if (currentOp[0] === null) {
+                        addDecimal(0)
+                    } else {
+                        addDecimal(0)
+                    }
+                }
+                break;
+            case "percent":
+                if (currentOp[1]) {
+                    percent()
+                }
+                break;
+            case "delete":
+                del();
+                break;
+            case "clear":
+                clear();
+                break;
+            case "calculate":
+                if (currentOp[1] !== null && currentOp[2] !== null) {
+                    calculate();
+                }
+                break;
+        }
+        lastType = type;
+    }
+}
+
+function percent() {
+    let num1 = +currentOp[0]
+    let operator = currentOp[1];
+    let num2 = +currentOp[2]
+    let num2AsPercent = num2 / 100;
+    let num2PercentOfNum1 = getPercentOfNumber(num2AsPercent, num1)
+    let result
+    switch (operator) {
+        case "x":
+            result = num2PercentOfNum1
+            break;
+        case "+":
+            result = num2PercentOfNum1 + num1;
+            break;
+        case "-":
+            result = num1 - num2PercentOfNum1;
+            break;
+        case "÷":
+            result = num2PercentOfNum1 * num1;
+            break;
+    }
+    if (isInteger(result)) {
+        result = Math.floor(result);
+    } else {
+        result.toFixed(2);
+    }
+    setAndClearCurrentOperation(result)
+}
+
+function getPercentOfNumber(percent, number) {
+    return (percent * number)
+}
+
+function addOperand(value) {
+    let indexValue = opsArray[opsIndex]
+    opsArray[opsIndex] = isArrayIndexValue(opsArray, opsIndex, undefined) ? value : indexValue + value;
+    if (isArrayIndexValue(currentOp, 1, null)){
+        currentOp[0] = opsArray[opsIndex]
+    } else {
+        currentOp[2] = opsArray[opsIndex]
+    }
+    syncDisplay()
+}
+
+function addOperator(value) {
+    opsArray.push(value)
+    if (isArrayIndexValue(currentOp, 0, null)) {
+        currentOp[0] = 0
+    }
+    opsIndex = opsArray.length - 1
+    currentOp[1] = opsArray[opsIndex]
+    opsIndex++
+    syncDisplay()
+}
+
+function addDecimal(index) {
+    currentOp[index] = currentOp[index] === null ? "0." : currentOp[index] += "."
+    opsArray[opsIndex] = opsArray[opsIndex] === undefined ? "0." : opsArray[opsIndex] += "."
+    syncDisplay()
+}
+
+function del() {
+    setLastTypeValue(currentOp, "not-null")
+    if (currentOp[0] === null) {
+        clear();
+    } else {
+        syncDisplay();
+    }
+    opsArray.pop();
+    opsIndex = opsArray.length - 1;
+}
+
+function clear() {
+    setAndClearCurrentOperation(null)
+    opsArray = []
+    clearDisplay()
+}
+
+function calculate() {
+    let num1 = +currentOp[0];
+    let operator = currentOp[1];
+    let num2 = +currentOp[2];
+    let result
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "÷":
+            if (num2 === 0) {
+                result = null
+                break;
+            }
+            result = divide(num1, num2)
+            break;
+        case "x":
+            result = multiply(num1, num2);
+            break;
+    }
+    if (result !== null) {
+        if (isInteger(result)) {
+            result = Math.floor(result);
+        }
+        setAndClearCurrentOperation(result)
+    } else {
+        dividedByZero = true;
+        displayDivisionError()
+    }
+
+}
+
+function displayDivisionError() {
+    clear()
+    display.textContent = pickRandomDivideByZeroMsg()
+    display.style.textAlign = "start"
+}
+
+function add(num1, num2) {
+    return (num1 + num2).toFixed(2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2).toFixed(2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2).toFixed(2);
+}
+
+function multiply(num1, num2) {
+    return (num1 * num2).toFixed(2);
+}
+
+function isNumber(char) {
+    return !isNaN(parseInt(char));
+}
+
+function syncDisplay() {
+    display.textContent = currentOp.join(" ");
+}
+
+function clearDisplay() {
+    display.textContent = 0;
+}
+
+function setLastTypeValue(array, type, value = null) {
+    array[findLastTypeValue(array, type)] = value
+}
+
+function findLastTypeValue(array, type) {
+    if (type === "not-null") {
+        for (let i = array.length - 1; i < array.length; i--) {
+            if (array[i] !== null) {
+                return i
+            }
+        }
+    } else if (type === "number") {
+        for (let i = array.length - 1; i < array.length; i--) {
+            if (isNumber(array[i])) {
+                return i
+            }
+        }
+    }
+
 }
 
 function isInteger(value) {
     return Number.isInteger(+value)
 }
 
-function updateOperationsArray(value, type) {
-    let indexValue = operationsArray[operationIndex]
-    if (indexValue === undefined) {
-        operationsArray.push(0)
-        indexValue = operationsArray[operationIndex]
-    }
-    const valueToPush = (type === "operand") ? +(indexValue + value) : value;
-    if (type === "operand") {
-        addOrEditOperand(valueToPush)
-    } else {
-        addOperator(value)
-
-    }
+function setAndClearCurrentOperation(value) {
+    currentOp[0] = value;
+    currentOp[1] = null;
+    currentOp[2] = null;
+    syncDisplay()
 }
 
-function addOperator(value) {
-    operationIndex++
-    operationsArray.push(value)
-    operationIndex++
+function pickRandomDivideByZeroMsg() {
+    return DIVIDE_BY_ZERO_MESSAGES[getRandomInt(0, DIVIDE_BY_ZERO_MESSAGES.length)]
 }
 
-function addOrEditOperand(value) {
-    operationsArray[operationIndex] = value;
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-function addToDisplay(value) {
-    displayValue = displayValue === "0" ? value : displayValue += value
-    display.textContent = displayValue;
-}
-
-function setDisplay(value) {
-    displayValue = value;
-    display.textContent = displayValue
-}
-
-function calculate() {
-    let operator = ""
-    let num2 = null
-    if (operationsArray.length > 0) {
-        setDisplay(operationsArray.reduce((sum, value) => {
-            if (isInteger(+value)) {
-                num2 = value;
-            } else {
-                operator = value;
-            }
-            if (isValidOperation(sum, operator, num2)) {
-                let result = operate(sum, operator, num2);
-                if (result === DIVIDE_BY_ZERO_MSG) {
-                    dividedByZero = true;
-                    return result;
-                } else {
-                    if (isInteger(result)) {
-                        sum = Math.floor(result);
-                    } else {
-                        sum = result.toFixed(2)
-                    }
-                    num1 = sum;
-                    operator = "";
-                    num2 = null;
-                }
-            }
-            return sum;
-        }))
-    }
-}
-
-function operate(num1, operator, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2).toFixed(2);
-        case '-':
-            return subtract(num1, num2).toFixed(2);
-        case 'x':
-            return multiply(num1, num2).toFixed(2);
-        case '÷':
-            if (num2 === 0) {
-                return "To infinity and beyond!"
-            }
-            return divide(num1, num2).toFixed(2);
-    }
-}
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function isValidOperation(num1, operator, num2) {
-    return (num1 !== null && operator !== "" && num2 != null)
-}
-
-function clear() {
-    num1 = 0
-    operationsArray = [];
-    operationIndex = 0;
-    dividedByZero = false
-    setDisplay("0")
+function isArrayIndexValue(array, index, value) {
+    return array[index] === value;
 }
 
 
